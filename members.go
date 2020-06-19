ed9a5e8a98a43dd35cc26e7ab7a671d4d588d481
Bubunyo Nyavor <samnyavor@gmail.com>
2020-06-19 19:23:13 +0000

    New Code

---
+++ members.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+)
+
+type Member struct {
+	ID          string
+	Name        string `json:"name"`
+	PhoneNumber string `json:"phoneNumber"`
+	Bank        string `json:"bank"`
+}
+
+var MemberMap = map[string]Member{}
+
+func CreateMember(w http.ResponseWriter, r *http.Request) {
+	var m Member
+	err := json.NewDecoder(r.Body).Decode(&m)
+	if err != nil {
+		ErrorResponse(w, err, http.StatusBadRequest)
+		return
+	}
+	m.ID = uuid.New().String()
+	MemberMap[m.ID] = m
+	JsonResponse(w, m)
+}
+
+func GetMembers(w http.ResponseWriter, r *http.Request) {
+	var ml []Member
+	for _, v := range MemberMap {
+		ml = append(ml, v)
+	}
+	JsonResponse(w, ml)
+}
+
+func GetMember(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	mID := vars["memberId"]
+	m, ok := MemberMap[mID]
+	if !ok {
+		ErrorResponse(w, fmt.Errorf("User Not Found"), http.StatusNotFound)
+	}
+	JsonResponse(w, m)
+}
+
+func UpdateMembers() {
+
+	// fetch a member by their user id and change the required parameter, return the result and save it
+
+}
+
+func DeleteMembers() {
+
+	// delete a member for our database
+
+}
